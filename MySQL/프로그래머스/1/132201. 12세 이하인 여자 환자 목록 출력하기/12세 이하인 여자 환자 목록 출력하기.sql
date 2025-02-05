67941a953b989bd61ad2101f40701b5bff64d7c1
yeedacoding <83167676+yeedacoding@users.noreply.github.com>
2025-02-05 11:23:31 +0900

    [level 1] Title: 12세 이하인 여자 환자 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
+++ MySQL/프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -1,5 +1,10 @@
 -- 코드를 입력하세요
-SELECT PT_NAME, PT_NO, GEND_CD, AGE, IFNULL(TLNO, 'NONE') TLNO
-FROM PATIENT
-WHERE AGE <= 12 AND GEND_CD = 'W'
-ORDER BY AGE DESC, PT_NAME;
+select PT_NAME,
+       PT_NO,
+       GEND_CD,
+       AGE,
+       IFNULL(TLNO, 'NONE') TLNO
+from PATIENT
+where AGE <= 12
+  and GEND_CD = 'W'
+order by AGE desc, PT_NAME;
