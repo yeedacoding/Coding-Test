bd7463bef527bc25beaa04c19de8a05e3d41e41a
yeedacoding <83167676+yeedacoding@users.noreply.github.com>
2025-02-10 15:35:34 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ MySQL/프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,4 +1,6 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-WHERE INTAKE_CONDITION = 'Sick';
+select ANIMAL_ID,
+       NAME
+from ANIMAL_INS
+where INTAKE_CONDITION = "Sick"
+order by ANIMAL_ID;
