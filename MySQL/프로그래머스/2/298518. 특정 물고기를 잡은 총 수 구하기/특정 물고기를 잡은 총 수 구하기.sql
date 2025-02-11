f18fecb2be3c1cca23b94294079643fa71ce2af7
yeedacoding <83167676+yeedacoding@users.noreply.github.com>
2025-02-11 14:49:29 +0900

    [level 2] Title: 특정 물고기를 잡은 총 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
+++ MySQL/프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 작성해주세요
-SELECT COUNT(ID) FISH_COUNT
-FROM fish_info fi
-JOIN fish_name_info fni
-ON fi.FISH_TYPE = fni.FISH_TYPE
-WHERE fni.FISH_NAME IN ('BASS', 'SNAPPER')
+select count(*) FISH_COUNT
+from FISH_INFO FI
+left join FISH_NAME_INFO FNI on FI.FISH_TYPE = FNI.FISH_TYPE
+where (FNI.FISH_NAME = 'BASS')
+   or (FNI.FISH_NAME = 'SNAPPER');
