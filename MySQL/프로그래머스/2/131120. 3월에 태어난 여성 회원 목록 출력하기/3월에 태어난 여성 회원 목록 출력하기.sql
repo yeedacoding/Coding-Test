b3ed69c443d09006829aba00469d0b8d3d0caedd
Tae Heon <taeheun0325@gmail.com>
2025-07-03 15:48:45 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ MySQL/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,10 +1,9 @@
--- 코드를 입력하세요
-select MEMBER_ID,
-       MEMBER_NAME,
-       GENDER,
-       DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') DATE_OF_BIRTH
-from MEMBER_PROFILE
-where DATE_OF_BIRTH like '%03%'
-      and TLNO is NOT NULL
-      and GENDER = 'W'
-order by MEMBER_ID;
+SELECT member_id,
+       member_name,
+       gender,
+       DATE_FORMAT(date_of_birth, '%Y-%m-%d') DATE_OF_BIRTH
+FROM member_profile
+WHERE date_of_birth LIKE '%-03-%'
+    AND gender = 'W'
+    AND tlno IS NOT NULL
+ORDER BY member_id;
