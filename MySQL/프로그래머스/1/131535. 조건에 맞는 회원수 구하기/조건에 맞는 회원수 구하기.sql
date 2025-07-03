477fcb4061b9ee2d0979adc4b6566c797a04a394
Tae Heon <taeheun0325@gmail.com>
2025-07-03 12:54:23 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
+++ MySQL/프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-select COUNT(USER_ID) USERS
-from USER_INFO
-where JOINED like "2021%"
-  and AGE between 20 and 29;
+SELECT COUNT(user_id)
+FROM user_info
+WHERE joined LIKE '2021%'
+    AND age BETWEEN 20 AND 29;
