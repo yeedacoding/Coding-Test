b21df20de361f21f10782ba53da9ddb90d726498
Tae Heon <taeheun0325@gmail.com>
2025-07-07 09:56:54 +0900

    [level 1] Title: 가장 비싼 상품 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
+++ MySQL/프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
@@ -2,3 +2,9 @@
 SELECT price AS MAX_PRICE
 FROM product
 WHERE price = (SELECT MAX(price) FROM product)
+
+-- 2. ORDER BY & LIMIT
+# SELECT price AS MAX_PRICE
+# FROM product
+# ORDER BY price DESC
+# LIMIT 1;
