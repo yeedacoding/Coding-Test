103fbc327f7f5e340756a400fccc670a0f84f300
Tae Heon <taeheun0325@gmail.com>
2025-07-07 09:50:30 +0900

    [level 2] Title: 가격이 제일 비싼 식품의 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
+++ MySQL/프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -1,8 +1,19 @@
+-- 1. SUBQUERY
 SELECT product_id,
        product_name,
        product_cd,
        category,
        price
 FROM food_product
-ORDER BY price DESC
-LIMIT 1;
+WHERE price = (SELECT MAX(price) FROM food_product)
+
+
+-- 2. ORDER BY & LIMIT
+# SELECT product_id,
+#        product_name,
+#        product_cd,
+#        category,
+#        price
+# FROM food_product
+# ORDER BY price DESC
+# LIMIT 1;
