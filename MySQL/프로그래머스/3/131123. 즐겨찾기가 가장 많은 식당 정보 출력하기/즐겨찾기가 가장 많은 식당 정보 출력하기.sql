8fe0eaca85ebfee4d37660b666293c180bcf101d
Tae Heon <taeheun0325@gmail.com>
2025-07-08 16:17:21 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ MySQL/프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,19 +1,20 @@
-# SELECT ri1.FOOD_TYPE,
-#        ri1.REST_ID,
-#        ri1.REST_NAME,
-#        ri2.max_fav FAVORITES
-# FROM rest_info ri1
-#     JOIN (
-#         SELECT rest_id, food_type, MAX(favorites) max_fav
-#         FROM rest_info
-#         GROUP BY food_type
-#     ) ri2
-#     ON ri1.rest_id = ri2.rest_id
-# ORDER BY ri1.food_type DESC;
+SELECT ri1.FOOD_TYPE,
+       ri1.REST_ID,
+       ri1.REST_NAME,
+       ri2.max_fav FAVORITES
+FROM rest_info ri1
+    JOIN (
+        SELECT food_type, MAX(favorites) max_fav
+        FROM rest_info
+        GROUP BY food_type
+    ) ri2
+    ON ri1.food_type = ri2.food_type
+    AND ri1.favorites = ri2.max_fav
+ORDER BY ri1.food_type DESC;
 
-SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
-FROM REST_INFO
-WHERE (FOOD_TYPE, FAVORITES) IN (SELECT FOOD_TYPE, MAX(FAVORITES)
-                              FROM REST_INFO
-                              GROUP BY FOOD_TYPE)
-ORDER BY FOOD_TYPE DESC ;
+# SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+# FROM REST_INFO
+# WHERE (FOOD_TYPE, FAVORITES) IN (SELECT FOOD_TYPE, MAX(FAVORITES)
+#                               FROM REST_INFO
+#                               GROUP BY FOOD_TYPE)
+# ORDER BY FOOD_TYPE DESC ;
